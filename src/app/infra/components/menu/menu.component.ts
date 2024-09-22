ffcdaaf6bfabe21372f70cac901178452ed5fab1
Mateus Gomez De Souza <mgsouza@fazenda.ms.gov.br>
2024-09-22 17:54:00 -0400

    Fechar automaticamente o menu quando clicar em algum ítem.

--- src/app/infra/components/menu/menu.component.ts
+++ src/app/infra/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatDrawer } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 
 @Component({
@@ -19,10 +20,13 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent {
 
+  @Input() drawer: MatDrawer;
+
   constructor(private router: Router) {
   }
 
   redirecionar(route: string) {
     this.router.navigate([route]);
+    this.drawer.close();
   }
 }
