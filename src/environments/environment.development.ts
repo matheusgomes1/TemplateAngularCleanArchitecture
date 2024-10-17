9b7f7fadddfa7a37872b9e64059385e40a927f67
--get <mgsouza@fazenda.ms.gov.br>
2024-10-17 19:02:00 -0400

    Adequação para textarea na descrição.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   environment: 'dev',
-  version: '1.0.0',
+  version: 'v1.0.0',
   apiUrl: 'http://localhost:8080/'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   environment: 'prod',
-  version: '1.0.0',
+  version: 'v1.0.0',
   apiUrl: 'https://api-for-template-angular-hvg6e0gtc5dpfkem.canadacentral-01.azurewebsites.net/'
 }
